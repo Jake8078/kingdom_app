355894bd36b1a642216357437c78ae1537a92989
Jake Ventrella <jake8078@hotmail.com>
2013-11-23 00:10:30 -0500

    Added citizens resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 KingdomApp::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  resources :citizens
+# root 'citizens#index'
+  
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
